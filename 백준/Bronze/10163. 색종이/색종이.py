46d52225f6359c7e8ccf159da67979db71910863
hyuna333 <122499274+hyuna333@users.noreply.github.com>
2023-03-01 22:07:25 +0900

    [Bronze I] Title: 색종이, Time: 288 ms, Memory: 38304 KB -BaekjoonHub

--- 백준/Bronze/10163. 색종이/색종이.py
+++ 백준/Bronze/10163. 색종이/색종이.py
@@ -3,19 +3,22 @@ import sys
 N = int(sys.stdin.readline())
 arr = [[0]*1001 for _ in range(1001)]
 
+si = sj = 1001
+ei = ej = 0
 for num in range(1, N+1):
-    sj, si, ga, se = map(int, sys.stdin.readline().split())
-    for i in range(si, si+se):
-        for j in range(sj, sj+ga):
+    y, x, ga, se = map(int, sys.stdin.readline().split())
+    for i in range(x, x+se):
+        for j in range(y, y+ga):
             arr[i][j] = num
+            if si > i: si = i
+            if sj > j: sj = j
+            if ei < i: ei = i
+            if ej < j: ej = j
 
 ans = [0]*(N+1)
-for i in range(1001):
-    for j in range(1001):
-        if arr[i][j] == 0: pass
-        else:
-            ans[arr[i][j]] += 1
-cnt = ans[1:]
-
-for i in range(len(cnt)):
-    print(cnt[i])
+for n in range(1,N+1):
+    for i in range(si, ei+1):
+        ans[n] += arr[i].count(n)
+        
+for i in range(1, len(ans)):
+    print(ans[i])
