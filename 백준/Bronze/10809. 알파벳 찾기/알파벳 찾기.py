dba4ddbb8277b787fb4f5f45154ea8df3478aca4
hyuna333 <122499274+hyuna333@users.noreply.github.com>
2023-08-31 21:04:25 +0900

    [Bronze II] Title: 알파벳 찾기, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.py
+++ 백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.py
@@ -1,13 +1,10 @@
-S = list(input())
-alpa = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
-        'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
-dct = {}
-for i in range(len(S)):
-    if S[i] not in dct.keys():
-        dct[S[i]] = i
+lst = list(input())
 
-for st in alpa:
-    if st not in S:
-        print(-1, end=' ')
-    else:
-        print(dct[st], end=' ')
+dct = {chr(i):-1 for i in range(97, 123)}
+
+for i in range(len(lst)):
+    if dct[lst[i]] == -1:
+        dct[lst[i]] = i
+
+for n in dct.values():
+    print(n, end=' ')
