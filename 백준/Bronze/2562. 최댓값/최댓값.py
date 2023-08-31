a7b8f4bdb7440f340e1705fd2a2115793b65494f
hyuna333 <122499274+hyuna333@users.noreply.github.com>
2023-08-31 21:13:49 +0900

    [Bronze III] Title: 최댓값, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/2562. 최댓값/최댓값.py
+++ 백준/Bronze/2562. 최댓값/최댓값.py
@@ -1,12 +1,9 @@
 lst = [int(input()) for _ in range(9)]
 
 mx = max(lst)
-ans = 0
 
 for i in range(9):
     if lst[i] == mx:
-        ans = i+1
+        print(mx)
+        print(i+1)
         break
-
-print(mx)
-print(ans)
