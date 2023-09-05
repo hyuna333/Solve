40cd1e87c82399a05642d581d0a475e05e321003
hyuna333 <122499274+hyuna333@users.noreply.github.com>
2023-09-05 20:15:12 +0900

    [Bronze II] Title: Hashing, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/15829. Hashing/Hashing.py
+++ 백준/Bronze/15829. Hashing/Hashing.py
@@ -1,10 +1,8 @@
-dct = {chr(i):i-96 for i in range(97, 123)}
-
 N = int(input())
 st = input()
 
 sm = 0
 for i in range(N):
-    sm += dct[st[i]] * (31 ** i)
+    sm += (ord(st[i])-96) * (31 ** i)
 
 print(sm)
