58cb41a67e18d30c2e0f49e30594f098a6468c89
hyuna333 <122499274+hyuna333@users.noreply.github.com>
2023-09-05 21:10:01 +0900

    [Bronze I] Title: 부녀회장이 될테야, Time: 52 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/2775. 부녀회장이 될테야/부녀회장이 될테야.py
@@ -0,0 +1,13 @@
+T = int(input())
+
+for _ in range(T):
+    k = int(input())
+    n = int(input())
+
+    apart = [i for i in range(1, n+1)]
+
+    for _ in range(k):
+        for i in range(1, n):
+            apart[i] += apart[i-1]
+
+    print(apart[-1])
