17e0f01f921be1350a90b08c97e1fdb6ba07fba0
hyuna333 <122499274+hyuna333@users.noreply.github.com>
2024-11-21 23:19:58 +0900

    [Silver IV] Title: 피보나치 수 7, Time: 212 ms, Memory: 31120 KB, Score: 201 point -BaekjoonHub

--- 백준/Silver/15624. 피보나치 수 7/피보나치 수 7.py
+++ 백준/Silver/15624. 피보나치 수 7/피보나치 수 7.py
@@ -3,9 +3,12 @@ input = sys.stdin.readline
 
 n = int(input())
 
-a, b = 0, 1  
-
-for _ in range(2, n + 1):
-    a, b = b, (a + b) % 1000000007
-
-print(b)
+if n == 0:
+    print(0)
+else:
+    a, b = 0, 1
+    
+    for _ in range(2, n + 1):
+        a, b = b, (a + b) % 1000000007
+    
+    print(b)
