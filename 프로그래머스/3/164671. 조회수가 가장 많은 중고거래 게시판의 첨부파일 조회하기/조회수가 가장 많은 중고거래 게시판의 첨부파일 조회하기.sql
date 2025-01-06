e9efac1885633e6007061eab2327bb22ebdde951
hyuna333 <122499274+hyuna333@users.noreply.github.com>
2025-01-06 23:37:08 +0900

    [level 3] Title: 조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/3/164671. 조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기/조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기.sql
@@ -0,0 +1,5 @@
+select CONCAT('/home/grep/src/', BOARD_ID, '/', FILE_ID, FILE_NAME, FILE_EXT) AS FILE_PATH
+from USED_GOODS_FILE
+where BOARD_ID = (select BOARD_ID from USED_GOODS_BOARD 
+                  order by VIEWS desc limit 1)
+order by FILE_ID desc;
